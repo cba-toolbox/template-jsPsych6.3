121b21a17432cee65ae16aa4263f44805acba51c
Yoshihiko Kunisato <ykunisato@psy.senshu-u.ac.jp>
2021-09-18 17:38:35 +0900

    init

---
+++ name_of_repository/jatos_jspsych_init.js
@@ -0,0 +1,10 @@
+jatos.onLoad(function() {
+    jsPsych.init( {
+    　timeline: timeline,
+      on_finish: function() {
+        var resultJson = jsPsych.data.get().json();
+        jatos.submitResultData(resultJson, jatos.startNextComponent);
+      }
+    });
+  });
+  
